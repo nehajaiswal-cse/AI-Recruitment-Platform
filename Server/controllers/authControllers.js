01c65df7ff9605086e71bfbb5aa3d5d13cc9257b
Asmit102 <asmitm750@gmail.com>
2026-08-12 22:16:30 +0530

    Add logout API

--- Server/controllers/authControllers.js
+++ Server/controllers/authControllers.js
@@ -117,3 +117,15 @@ export const login = async (req, res) => {
     });
   }
 };
+export const logout = async (req, res) => {
+  try {
+    res.status(200).json({
+      message: "Logout successful",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
+  }
+};
--- Server/routes/protectedRoutes.js
+++ Server/routes/protectedRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
+import roleMiddleware from "../middlewares/roleMiddleware.js";
 
 const router = express.Router();
 
@@ -9,5 +10,17 @@ router.get("/profile", authMiddleware, (req, res) => {
     user: req.user,
   });
 });
+
+router.get(
+  "/recruiter-dashboard",
+  authMiddleware,
+  roleMiddleware("recruiter"),
+  (req, res) => {
+    res.json({
+      message: "Welcome Recruiter!",
+      user: req.user,
+    });
+  }
+);
 
 export default router;
