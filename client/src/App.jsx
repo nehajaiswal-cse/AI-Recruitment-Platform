dd2fcc113288b84922ff551d5c24a27e6a4ab9db
nehajaiswal-cse <nehajaiawal840@gmail.com>
2026-08-15 00:43:34 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
       <Routes>
          <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} /> */}
+       {/* <Route path="/forgot-password" element={<ForgotPassword />} />  */}
          <Route path="/applicant" element={<ADashboard />} />
         <Route path="/" element={<Home/>} />
         <Route path="/recruiter" element={<RDashboard />} />
