97109602ce10bd5b2f53dd4f807910d9f9e2050b
nehajaiswal-cse <nehajaiawal840@gmail.com>
2026-08-15 15:11:17 +0530

    Clean up import statements in main.jsx
    Removed duplicate import statements for React and BrowserRouter.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-
-import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import AuthContextProvider from "./context/AuthContext.jsx";
 
