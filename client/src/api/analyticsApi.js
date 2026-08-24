703c4b5753bee47a458eb7d0df2719700ab8ccb9
nehajaiswal-cse <nehajaiawal840@gmail.com>
2026-08-24 23:54:03 +0530

    Update analyticsApi.js

--- client/src/api/analyticsApi.js
+++ client/src/api/analyticsApi.js
@@ -1,15 +1,7 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:5000/api/analytics";
+import api from "./api";
 
 export const getRecruiterAnalytics = async () => {
-  const token = localStorage.getItem("token");
-
-  const response = await axios.get(`${API_URL}/recruiter`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  const response = await api.get("/analytics/recruiter");
 
   return response.data;
 };
