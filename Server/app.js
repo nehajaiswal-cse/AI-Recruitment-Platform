12cf01ac3e00002a7586cb890a441f20d0ba8ba1
nehajaiswal-cse <nehajaiawal840@gmail.com>
2026-09-18 09:56:25 +0530

    Add CLIENT_URL to CORS origin list

--- Server/app.js
+++ Server/app.js
@@ -20,7 +20,7 @@ import copilotRoutes from "./routes/copilotRoutes.js"
 const app = express();
 
 app.use(cors({ 
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "http://localhost:5174",process.env.CLIENT_URL],
   credentials: true 
 }))
 
